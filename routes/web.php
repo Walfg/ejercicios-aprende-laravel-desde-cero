494a8d9036e3981756acd76d7cdbb25edaf5ac1a
Cris <cris2003a@gmail.com>
2023-10-27 16:40:06 +0200

    Solution to B

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 //     return dd($request);
 // });
 
+//A
 Route::post('/ejercicio2/a', function (Request $request) {
     return FacadesResponse::json([
         "name" => $request->get("name"),
@@ -17,3 +18,13 @@ Route::post('/ejercicio2/a', function (Request $request) {
         "price" => $request->get("price")
     ]);
 });
+
+//B
+Route::post('/ejercicio2/b', function (Request $request) {
+    if ($request->get("price") < 0){
+        return FacadesResponse::json(["message" => "Price can't be less than 0"])->setStatusCode(422);
+    }
+});
+
+
+
