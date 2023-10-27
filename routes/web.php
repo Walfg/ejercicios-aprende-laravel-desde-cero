930ccb511d87cf169bea3befee927e17201db49a
Cris <cris2003a@gmail.com>
2023-10-27 17:53:08 +0200

    C solved. Exercise complete

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 //A
-Route::post('/ejercicio2/a', function (Request $request) {
+Route::post("/ejercicio2/a", function (Request $request) {
     return FacadesResponse::json([
         "name" => $request->get("name"),
         "description" => $request->get("description"),
@@ -20,11 +20,53 @@ Route::post('/ejercicio2/a', function (Request $request) {
 });
 
 //B
-Route::post('/ejercicio2/b', function (Request $request) {
-    if ($request->get("price") < 0){
+Route::post("/ejercicio2/b", function (Request $request) {
+    if ($request->get("price") < 0) {
         return FacadesResponse::json(["message" => "Price can't be less than 0"])->setStatusCode(422);
     }
 });
 
+//C
+Route::post("/ejercicio2/c", function (Request $request) {
+    if (isset($request->query()["discount"]) && $request->query("discount") > 0) {
+        switch ($request->query("discount")) {
+            case "SAVE5":
+                //Descuento del 5% encontrado
+                $discount = 5;
+                $price = $request->get("price") * (100 - $discount) / 100;
+                break;
 
+            case "SAVE10":
+                //Descuento del 10% encontrado
+                $discount = 10;
+                $price = $request->get("price") * (100 - $discount) / 100;
+                break;
 
+            case "SAVE15":
+                //Descuento del 15% encontrado
+                $discount = 15;
+                $price = $request->get("price") * (100 - $discount) / 100;
+                break;
+
+            default:
+                //Si el cupón existe pero no es válido
+                $discount = 0;
+                $price = $request->get("price");
+                break;
+        }
+
+        return FacadesResponse::json([
+            "name" => $request->get("name"),
+            "description" => $request->get("description"),
+            "price" => $price,
+            "discount" => $discount
+        ]);
+    } else {
+        return FacadesResponse::json([
+            "name" => $request->get("name"),
+            "description" => $request->get("description"),
+            "price" => $request->get("price"),
+            "discount" => 0
+        ]);
+    }
+});
